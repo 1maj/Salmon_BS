d9f432e271ea20051015461dce8442c6197b0ce2
1Maj <45659126+1maj@users.noreply.github.com>
2018-12-11 14:54:10 +0100

    Folde structure
    Updated folder structure to ease upload of mod

---
+++ addons/salmon_bs/config.cpp
@@ -0,0 +1,23 @@
+class CfgPatches{
+	class Salmon_bs_main{
+		name = "Salmon ButtStroke";
+		requiredVersion=1.88;
+		requiredAddons[]={
+			"ace_medical",
+			"cba_keybinding",
+			"cba_main",
+			"cba_main_a3",
+			"cba_settings",
+			"extended_eventhandlers"
+		};
+		author="Green";
+		units[]={};
+		weapons[]={};
+		authorUrl="http://TacSalmon.me";
+	};
+};
+class Extended_PreInit_EventHandlers{
+	class Salmon_BS{
+		init="call compile preprocessFileLineNumbers 'Salmon_BS\XEH_PreInit.sqf'";
+	};
+};
